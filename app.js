8b0ee4db9f575d624541b425e710f4f2ea9843b2
Foluso Ojo <folusoojo96@gmail.com>
2024-10-10 21:12:36 +0100

    responsiveness

--- app.js
+++ app.js
@@ -10,16 +10,22 @@ btn.addEventListener("click", () => {
   searchInput.textContent = "";
   let search = searchInput.value;
   console.log(search);
-  
+  ipdetails.style.width = "65%";
 
-  fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_D8CKBbVOOmN6WEzLqi01Ok5WhY8ZX&ipAddress=${search}`)
+  fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=at_D8CKBbVOOmN6WEzLqi01Ok5WhY8ZX&ipAddress=${search}`
+  )
     .then((data) => data.json())
     .then((data) => {
       console.log(data);
-      Address.textContent = data.ip
-      loca.textContent = data.location.region + ", " + data.location.country + ", " + data.as.asn
-      timeZone.textContent = data.location.timezone
-      ISP.textContent = data.isp
-      
+      Address.textContent = data.ip;
+      loca.textContent =
+        data.location.region +
+        ", " +
+        data.location.country +
+        ", " +
+        data.as.asn;
+      timeZone.textContent = data.location.timezone;
+      ISP.textContent = data.isp;
     });
 });
