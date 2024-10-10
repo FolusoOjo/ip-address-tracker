7863ad04a4b9b3dad63eb0f69d28d5d31b7f8f0b
Yungflashh <streaktv8@gmail.com>
2024-10-10 17:11:40 +0100

    i did the js

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const searchInput = document.getElementById("searchInput");
+const btn = document.getElementById("btn");
 const ipdetails = document.getElementById("ipdetails");
+const Address = document.getElementById("Address");
+const loca = document.getElementById("location");
+const timeZone = document.getElementById("timeZone");
+const ISP = document.getElementById("ISP");
 
-searchInput.addEventListener("click", () => {
-  ipdetails.innerHTML = "";
+btn.addEventListener("click", () => {
+  searchInput.textContent = "";
   let search = searchInput.value;
+  console.log(search);
+  
 
-  fetch(`http://ip-api.com/json/{search}`)
+  fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_D8CKBbVOOmN6WEzLqi01Ok5WhY8ZX&ipAddress=${search}`)
     .then((data) => data.json())
-    .then((data) => {});
+    .then((data) => {
+      console.log(data);
+      Address.textContent = data.ip
+      loca.textContent = data.location.region + ", " + data.location.country + ", " + data.as.asn
+      timeZone.textContent = data.location.timezone
+      ISP.textContent = data.isp
+      
+    });
 });
